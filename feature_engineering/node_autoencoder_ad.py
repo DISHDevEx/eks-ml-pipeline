aab1be7121ecc91ef5c0a44f13622c5fb82f543c
praveen mada <praveen.mada@dish.com>
2022-11-23 16:08:39 +0000

    updates to feature engineering flow

--- feature_engineering/node_autoencoder_ad.py
+++ feature_engineering/node_autoencoder_ad.py
@@ -10,12 +10,25 @@ def node_autoencoder_ad_preprocessing(feature_group_name, feature_group_created_
     if err == 'PASS':
         features_df = get_features(feature_group_name,feature_group_created_date)
         features = features_df["feature_name"].to_list()
-        #['node_cpu_utilization','node_memory_utilization','node_network_total_bytes']
+        print(features)
         node_df = node_df. withColumn("Timestamp",(col("Timestamp")/1000).cast("timestamp")).select("Timestamp","InstanceId","node_cpu_utilization","node_memory_utilization","node_network_total_bytes")
         node_df = node_df.na.drop(subset=["node_cpu_utilization","node_memory_utilization","node_network_total_bytes"])
-        
         return node_df
-    
     else:
         empty_df = pd.DataFrame()
         return empty_df
+    
+    
+
+def node_autoencoder_ad_feature_engineering(input_processed_df):
+    
+    featured_engineered_df  = input_processed_df
+    return featured_engineered_df
+
+
+
+
+def write_to_s3(input_featured_engineered_df):
+    return 0
+    
+    
--- run.py
+++ run.py
@@ -1,5 +1,10 @@
-from feature_engineering import node_autoencoder_ad_preprocessing
+from feature_engineering import node_autoencoder_ad_preprocessing, node_autoencoder_ad_feature_engineering, write_to_s3
 import pandas as pd
 
-data = node_autoencoder_ad_preprocessing("node_autoencoder_ad_features","11-21-2022","2022","10","10","10")
-print(data.show(truncate=False))
+processed_data = node_autoencoder_ad_preprocessing("node_autoencoder_ad_features","11-21-2022","2022","10","10","10")
+
+training_data = node_autoencoder_ad_feature_engineering(processed_data)
+
+print(training_data.show(truncate=False))
+
+write_to_s3(training_data)
