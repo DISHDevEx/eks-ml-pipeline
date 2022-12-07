efa0de48c0572cd53d65ebbcb8abd98a2406236d
Evgeniya Dontsova <evgeniya.dontsova@dish.com>
2022-12-07 21:15:05 +0000

    test pod fe

--- eks_ml_pipeline/feature_engineering/pod_autoencoder_ad.py
+++ eks_ml_pipeline/feature_engineering/pod_autoencoder_ad.py
@@ -8,7 +8,44 @@ from pyspark.sql.functions import col, count, rand, row_number, get_json_object
 from pyspark.ml.feature import VectorAssembler, StandardScaler
 from pyspark.ml import Pipeline
 
+"""
+Contributed by Madhu Bandi, Evgeniya Dontsova and Praveen Mada
+MSS Dish 5g - Pattern Detection
+
+this feature engineering functions will help us run bach jobs that builds training data for Anomaly Detection models
+"""
+
 def pod_autoencoder_ad_preprocessing(feature_group_name, feature_group_created_date, input_year, input_month, input_day, input_hour, input_setup = "default"):
+    """
+    inputs
+    ------
+            feature_group_name: STRING
+            json name to get the required features
+            
+            feature_group_created_date: STRING
+            json created date to get the latest features 
+            
+            input_year : STRING | Int
+            the year from which to read data, leave empty for all years
+
+            input_month : STRING | Int
+            the month from which to read data, leave empty for all months
+
+            input_day : STRING | Int
+            the day from which to read data, leave empty for all days
+
+            input_hour: STRING | Int
+            the hour from which to read data, leave empty for all hours
+            
+            input_setup: STRING 
+            kernel config
+    
+    outputs
+    -------
+            features_df : processed features dataFrame
+            final_pod_df: pre processed node dataframe
+            
+    """
 
     pod_data = Pyspark_data_ingestion(year = input_year, month = input_month, day = input_day, hour = input_hour, setup = input_setup, filter_column_value ='Pod')
     err, pod_df = pod_data.read()
@@ -88,11 +125,12 @@ def pod_autoencoder_ad_feature_engineering(input_pod_features_df, input_pod_proc
     final_pod_fe_df = None
     
     input_pod_processed_df.persist(StorageLevel.MEMORY_ONLY)
-
-    for n in range(n_samples):
+    #for n in range(n_samples):
+    n = 0;
+    while n < n_samples:
+        
         ##pick random df, and normalize
         random_instance_id= random.choice(input_pod_processed_df.select("pod_id").rdd.flatMap(list).collect())
-        print(random_instance_id)
         pod_fe_df = input_pod_processed_df[(input_pod_processed_df["pod_id"] == random_instance_id)][["Timestamp", "pod_id"] + features].select('*')
         pod_fe_df = pod_fe_df.sort("Timestamp")
         pod_fe_df = pod_fe_df.na.drop(subset=features)
@@ -102,11 +140,17 @@ def pod_autoencoder_ad_feature_engineering(input_pod_features_df, input_pod_proc
         scaler = StandardScaler(inputCol = "vectorized_features", outputCol = "scaled_features", withMean=True, withStd=True)
         pipeline = Pipeline(stages=[assembler, scaler])
         pod_fe_df = pipeline.fit(pod_fe_df).transform(pod_fe_df)
+        
+        #fix negative number bug 
+        if pod_fe_df.count()-time_steps <= 0:
+            break
 
         #tensor builder
         start = random.choice(range(pod_fe_df.count()-time_steps))
         pod_tensor_df = pod_fe_df.withColumn('rn', row_number().over(Window.partitionBy("pod_id").orderBy("Timestamp"))).filter((col("rn") >= start) & (col("rn") < start+time_steps)).select("scaled_features")
         pod_tensor_list = pod_tensor_df.select("scaled_features").rdd.flatMap(list).collect()
+        
+        #fix shape mismatch bug
         if len(pod_tensor_list) == time_steps:
             pod_tensor[n,:,:] = pod_tensor_list
 
@@ -115,7 +159,10 @@ def pod_autoencoder_ad_feature_engineering(input_pod_features_df, input_pod_proc
             else:
                 final_pod_fe_df = final_pod_fe_df.union(pod_fe_df)
         else:
-            n_samples = n_samples+1
+            #n_samples = n_samples+1
+            break
+            
+        n += 1
  
     final_pod_fe_df = final_pod_fe_df.select("Timestamp","pod_id",*features,"scaled_features")
     
