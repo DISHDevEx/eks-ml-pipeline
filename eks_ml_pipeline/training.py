b9559b7889921ffa77ac56e4fe34f75628e2d56a
Evgeniya Dontsova <evgeniya.dontsova@dish.com>
2022-12-09 22:17:47 +0000

    minor updates

--- eks_ml_pipeline/training.py
+++ eks_ml_pipeline/training.py
@@ -2,29 +2,32 @@ import numpy as np
 from msspackages import get_features
 from models import autoencoder_model_dish_5g 
 
-def pod_autoencoder_training(pod_training_tensor, 
-                             feature_group_name, 
-                             feature_group_created_date, 
-                             save_model_path = '/root/CodeCommit/trained_models/pod_autoencoder'):
+def autoencoder_training(training_tensor, 
+                         feature_group_name, 
+                         feature_group_created_date, 
+                         save_model_path):
     
-    pod_features_df = get_features(feature_group_name,feature_group_created_date)
-    model_parameters = pod_features_df["model_parameters"].iloc[0]
+    features_df = get_features(feature_group_name,feature_group_created_date)
+    model_parameters = features_df["model_parameters"].iloc[0]
     
     #Initialize autoencoder model
-    pod_autoencoder = autoencoder_model_dish_5g(time_steps=model_parameters["time_steps"], 
-                                                batch_size=model_parameters["batch_size"])
+    autoencoder = autoencoder_model_dish_5g(time_steps=model_parameters["time_steps"], 
+                                            batch_size=model_parameters["batch_size"])
     
     #Train model
-    pod_autoencoder.train(pod_training_tensor)
+    autoencoder.fit(training_tensor)
     
     #Save model
-    pod_autoencoder.save_nn(save_model_path)
+    autoencoder.save_nn(save_model_path)
         
-    return pod_autoencoder
+    return autoencoder
 
 
 if __name__ == "__main__":
     #load training data
     pod_training_tensor = np.loadtxt('/root/CodeCommit/data/pod_training_tensor.txt').reshape(600,12,3)
     
-    pod_autoencoder_training(pod_training_tensor, "pod_autoencoder_ad", "11-30-2022")
+    pod_autoencoder = autoencoder_training(pod_training_tensor, 
+                                           "pod_autoencoder_ad", 
+                                           "11-30-2022", 
+                                           '/root/CodeCommit/trained_models/pod_autoencoder')
