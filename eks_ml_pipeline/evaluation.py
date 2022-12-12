9ea377ed2157eacca4b8dce564c371c33459ae38
Evgeniya Dontsova <evgeniya.dontsova@dish.com>
2022-12-12 18:07:24 +0000

    update training.py

--- eks_ml_pipeline/evaluation.py
+++ eks_ml_pipeline/evaluation.py
@@ -1,17 +1,17 @@
 import numpy as np
-from models import autoencoder_model_dish_5g 
-#from fastparquet import write
+import tensorflow as tf
+from eks_ml_pipeline import autoencoder_model_dish_5g 
 
-def pod_autoencoder_testing(pod_testing_tensor, model_path = '/root/CodeCommit/trained_models/pod_autoencoder'):
+def autoencoder_testing(testing_tensor, model_path = '/root/CodeCommit/trained_models/pod_autoencoder'):
     
     #Load trained model
-    pod_autoencoder = tf.keras.models.load_model(model_path)
+    autoencoder = tf.keras.models.load_model(model_path)
     
     #Make predictions
-    test_predictions = pod_autoencoder.predict(pod_testing_tensor)
+    test_predictions = autoencoder.predict(testing_tensor)
     
     #Calculate residuals for testing data == anomaly score
-    test_residuals = np.abs(test_predictions - pod_testing_tensor)
+    test_residuals = np.abs(test_predictions - testing_tensor)
     
     return test_predictions, test_residuals
     
@@ -21,8 +21,5 @@ if __name__ == "__main__":
     #load testing data
     pod_testing_tensor = np.loadtxt('/root/CodeCommit/data/pod_training_tensor_2.txt').reshape(600,12,3)
     
-    test_predictions, test_residuals = pod_autoencoder_testing(pod_testing_tensor)
-    
-    # #save test_predictions and test_residuals in parquette format
-    # write('/root/CodeCommit/data/test_predictions.parq', test_predictions)
-    # write('/root/CodeCommit/data/test_residuals.parq', test_residuals)
+    test_predictions, test_residuals = autoencoder_testing(pod_testing_tensor)
+ 
--- eks_ml_pipeline/training.py
+++ eks_ml_pipeline/training.py
@@ -1,13 +1,48 @@
 import numpy as np
+import boto3
+from io import BytesIO
 from msspackages import get_features
-from models import autoencoder_model_dish_5g 
+from eks_ml_pipeline import read_tensor, uploadDirectory, autoencoder_model_dish_5g
+
+"""
+Contributed by Evgeniya Dontsova
+MSS Dish 5g - Pattern Detection
+
+this model training functions will be used to train and save Anomaly Detection models
+"""
+
 
 def autoencoder_training(training_tensor, 
                          feature_group_name, 
-                         feature_group_created_date, 
-                         save_model_path):
+                         feature_input_version, 
+                         save_model_local_path):
     
-    features_df = get_features(feature_group_name,feature_group_created_date)
+    """
+    inputs
+    ------
+            training_tensor: np.array
+            tensor requires as model input for training
+
+            feature_group_name: str
+            json name to get the required features
+            
+            feature_input_version: str
+            json version to get the latest features 
+            
+            save_model_local_path: str
+            local path to save trained model 
+            (by default it is one level above git repo)
+    
+    
+    outputs
+    -------
+            autoencoder: keras model object
+            trained keras model
+            
+    """
+
+    
+    features_df = get_features(feature_group_name, feature_input_version)
     model_parameters = features_df["model_parameters"].iloc[0]
     
     #Initialize autoencoder model
@@ -18,16 +53,164 @@ def autoencoder_training(training_tensor,
     autoencoder.fit(training_tensor)
     
     #Save model
-    autoencoder.save_nn(save_model_path)
+    autoencoder.save_nn(save_model_local_path)
         
     return autoencoder
 
 
-if __name__ == "__main__":
-    #load training data
-    pod_training_tensor = np.loadtxt('/root/CodeCommit/data/pod_training_tensor.txt').reshape(600,12,3)
+def autoencoder_training_pipeline(feature_group_name, feature_input_version,
+                                  data_bucketname, data_filename,
+                                  save_model_local_path, model_bucketname,
+                                  model_name, model_version):
     
-    pod_autoencoder = autoencoder_training(pod_training_tensor, 
-                                           "pod_autoencoder_ad", 
-                                           "11-30-2022", 
-                                           '/root/CodeCommit/trained_models/pod_autoencoder')
+    """
+    inputs
+    ------
+            feature_group_name: str
+            json name to get the required features
+            
+            feature_input_version: str
+            json version to get the latest features 
+            
+            data_bucketname: str
+            s3 bucket name where training data is saved
+            
+            data_filename: str
+            filename where training data is saved 
+
+            save_model_local_path: str
+            local path to save trained model 
+            (by default it is one level above git repo)
+            
+            model_bucketname: str
+            s3 bucket name where trained model will be saved
+            
+            model_name: str
+            filename where training model will be saved
+
+            model_version: str
+            training model version
+
+    
+    outputs
+    -------
+            autoencoder: keras model object
+            trained keras model
+            
+    """
+
+        
+    ###Load training data: read from s3 bucket
+    training_tensor = read_tensor(data_bucketname,
+                                  data_filename)
+    
+    ####Train autoencoder model
+    autoencoder = autoencoder_training(training_tensor, 
+                                       feature_group_name, 
+                                       feature_input_version, 
+                                       save_model_local_path)
+
+
+    ####Save model object to s3 bucket
+    uploadDirectory(local_path = save_model_local_path,
+                    bucketname = model_bucketname,
+                    model_name = model_name,
+                    version = model_version)
+    
+
+####################################################
+###******** AUTOENCODER MODEL PARAMETERS******** ###
+####################################################
+
+def node_autoencoder_input():
+    
+    """
+    outputs
+    -------
+            list of parameters for node rec type
+            required by autoencoder model 
+            training pipeline
+            
+    """
+
+    
+    feature_group_name = "node_autoencoder_ad"
+    feature_input_version = "v0.0.1"  
+    data_bucketname = 'mss-shared'
+    data_filename = 'x_train_36k_sample.npy'
+    
+    save_model_local_path = "../node_autoencoder"
+    model_bucketname = 'emr-serverless-output-pd'
+    model_name = 'node_autoencoder_test'
+    model_version = 'v0.0.1'
+    
+    return [feature_group_name, feature_input_version,
+            data_bucketname, data_filename,
+            save_model_local_path, model_bucketname,
+            model_name, model_version]
+
+def pod_autoencoder_input():
+    
+    """
+    outputs
+    -------
+            list of parameters for pod rec type
+            required by autoencoder model 
+            training pipeline
+            
+    """
+
+    
+    feature_group_name = "pod_autoencoder_ad"
+    feature_input_version = "v0.0.1"  
+    data_bucketname = 'mss-shared'
+    data_filename = 'x_train_36k_sample.npy'
+    
+    save_model_local_path = "../pod_autoencoder"
+    model_bucketname = 'emr-serverless-output-pd'
+    model_name = 'pod_autoencoder_test'
+    model_version = 'v0.0.1'
+    
+    return [feature_group_name, feature_input_version,
+            data_bucketname, data_filename,
+            save_model_local_path, model_bucketname,
+            model_name, model_version]
+
+def container_autoencoder_input():
+    
+    """
+    outputs
+    -------
+            list of parameters for container rec type
+            required by autoencoder model 
+            training pipeline
+            
+    """
+
+    
+    feature_group_name = "container_autoencoder_ad"
+    feature_input_version = "v0.0.1"  
+    data_bucketname = 'mss-shared'
+    data_filename = 'x_train_36k_sample.npy'
+    
+    save_model_local_path = "../container_autoencoder"
+    model_bucketname = 'emr-serverless-output-pd'
+    model_name = 'container_autoencoder_test'
+    model_version = 'v0.0.1'
+    
+    return [feature_group_name, feature_input_version,
+            data_bucketname, data_filename,
+            save_model_local_path, model_bucketname,
+            model_name, model_version]
+
+    
+if __name__ == "__main__":
+    
+    #Train node autoencoder model and save on s3
+    autoencoder_training_pipeline(*node_autoencoder_input())
+    
+    #Train pod autoencoder model and save on s3
+    autoencoder_training_pipeline(*pod_autoencoder_input())
+
+    #Train container autoencoder model and save on s3
+    autoencoder_training_pipeline(*container_autoencoder_input())
