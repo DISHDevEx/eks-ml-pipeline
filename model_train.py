89cd0e3c179744a8d6e16d23be92e96a66a13ab6
madhu-bandi <madhu.bandi@dish.com>
2022-12-14 16:08:32 +0000

    cleanup

--- model_train.py
+++
@@ -1,9 +0,0 @@
-from modelling import Autoencoder_Model_Dish_5g
-from training_data_builder import pod_training_data_builder
-
-model = Autoencoder_Model_Dish_5g(batch_size = batch_size, time_steps = time_steps)
-val_err = model.train(x_train)
-    
-model.save('pod_model.hdf5')
-
-
