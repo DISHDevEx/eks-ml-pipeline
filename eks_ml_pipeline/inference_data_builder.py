66fbb17876d731ee34ddf32d765f946feb012eba
praveen <praveen.mada@dish.com>
2022-12-14 19:50:51 +0000

    inference data builder changes

--- eks_ml_pipeline/inference_data_builder.py
+++ eks_ml_pipeline/inference_data_builder.py
@@ -1,5 +1,7 @@
 from msspackages import Pyspark_data_ingestion
 import pandas as pd
+import awswrangler as wr
+    
 
 
 def inference_data_builder(input_year, input_month,  input_day, input_hour, rec_type, input_setup):
@@ -14,10 +16,10 @@ def inference_data_builder(input_year, input_month,  input_day, input_hour, rec_
     pyspark_data = Pyspark_data_ingestion(year = input_year, month = input_month, day = input_day, hour = input_hour, setup = input_setup, filter_column_value = rec_type)
     err, pyspark_df = pyspark_data.read()
     
-    #if err == 'PASS':
-    print(err)
-    pyspark_df = pyspark_df.select("*")
-    pyspark_df.write.mode('overwrite').parquet(f's3a://dish-5g.core.pd.g.dp.eks.logs.e/inference_data/{file_name}/')
+    if err == 'PASS':
+        print(err)
+        pyspark_df = pyspark_df.toPandas()
+        wr.s3.to_parquet(pyspark_df, path=f"s3://dish-5g.core.pd.g.dp.eks.logs.e/inference_data/{file_name}.parquet")
 
 
 if __name__ == "__main__":
