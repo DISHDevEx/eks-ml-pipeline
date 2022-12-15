7eec0c1eff06ee6dcff97814d5dc1c054a363b4c
Evgeniya Dontsova <evgeniya.dontsova@dish.com>
2022-12-15 06:52:20 +0000

    update init

--- eks_ml_pipeline/__init__.py
+++ eks_ml_pipeline/__init__.py
@@ -34,3 +34,7 @@ from .training_input import container_autoencoder_input
 from .training_input import node_pca_input
 from .training_input import pod_pca_input
 from .training_input import container_pca_input
+
+from .inference_input import node_inference_input
+from .inference_input import pod_inference_input
+from .inference_input import container_inference_input
