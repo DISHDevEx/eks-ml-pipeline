f121b2cfe5f894691f18928b00cb866e01a6d39c
David Cherney <david.cherney@dish.com>
2023-01-12 18:19:08 +0000

    linting of setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,29 @@
+"""
+Build the wheel file for the library eks-ml-pipeline.
+"""
+
 from setuptools import find_packages, setup
 
 setup(
     name='eks_ml_pipeline',
-    version='0.0.1',    
+    version='0.0.1',
     description='pipeline for eks',
     url='',
-    
     author= 'Hamza Khokhar, Praveen Mada',
     author_email='hamza.khokhar@dish.com, praveen.mada@dish.com',
     license='Dish Wireless',
-    packages=find_packages(include=['eks_ml_pipeline','eks_ml_pipeline.feature_engineering', 'eks_ml_pipeline.models', 'eks_ml_pipeline.utilities']),
+    packages=find_packages(include=['eks_ml_pipeline',
+                                    'eks_ml_pipeline.feature_engineering',
+                                    'eks_ml_pipeline.models',
+                                    'eks_ml_pipeline.utilities']),
     include_package_data=True,
-    #entry_points = {'console_scripts': ['msspackages=msspackages.project_inital_setu:setup_runner']}
-    #scripts=['msspackages/project_inital_setup/understanding_eks_setup.py'],
+#     entry_points = {
+#         'console_scripts': [
+#             'msspackages=msspackages.project_inital_setu:setup_runner',
+#         ]}
+#     scripts = [
+#         'msspackages/project_inital_setup/understanding_eks_setup.py'
+#         ],
     install_requires = [
         'pyspark',
         'pandas',
@@ -23,9 +34,8 @@ setup(
         'dill',
         'scikit_learn',
         'statsmodels'],
-    
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Dish Wireless',
-    ],
-)
+        ],
+    )
