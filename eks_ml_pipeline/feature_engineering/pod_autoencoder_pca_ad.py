aa73c2dc61355583097fdea5e5603f227bbc1110
David Cherney <david.cherney@dish.com>
2023-01-17 23:07:01 +0000

    converted training pipeline to a class

--- eks_ml_pipeline/feature_engineering/pod_autoencoder_pca_ad.py
+++ eks_ml_pipeline/feature_engineering/pod_autoencoder_pca_ad.py
@@ -20,10 +20,10 @@ def pod_ad_preprocessing(input_feature_group_name, input_feature_group_version, 
     ------
             input_feature_group_name: STRING
             json name to get the required features
-            
+
             input_feature_group_version: STRING
-            json version to get the latest features 
-            
+            json version to get the latest features
+
             input_year : STRING | Int
             the year from which to read data, leave empty for all years
 
@@ -35,15 +35,15 @@ def pod_ad_preprocessing(input_feature_group_name, input_feature_group_version, 
 
             input_hour: STRING | Int
             the hour from which to read data, leave empty for all hours
-            
-            input_setup: STRING 
+
+            input_setup: STRING
             kernel config
-    
+
     outputs
     -------
             features_df : processed features dataFrame
             final_pod_df: pre processed pod dataframe
-            
+
     """
 
     pod_data = EKS_Connector(year = input_year, month = input_month, day = input_day, hour = input_hour, setup = input_setup, filter_column_value ='Pod')
@@ -54,7 +54,7 @@ def pod_ad_preprocessing(input_feature_group_name, input_feature_group_version, 
         features_df = get_features(input_feature_group_name,input_feature_group_version)
         features = features_df["feature_name"].to_list()
         processed_features = feature_processor.cleanup(features)
-        
+
         model_parameters = features_df["model_parameters"].iloc[0]
         time_steps = model_parameters["time_steps"]
 
@@ -64,23 +64,23 @@ def pod_ad_preprocessing(input_feature_group_name, input_feature_group_version, 
                            "pod_status", *processed_features)
         pod_df = pod_df.withColumn("Timestamp",(col("Timestamp")/1000).cast("timestamp"))
         cleaned_pod_df = pod_df.na.drop(subset=processed_features)
-        
+
         #Quality(timestamp filtered) pods
         cleaned_pod_df = cleaned_pod_df.filter(col("pod_status") == "Running")
         quality_filtered_pod_df = cleaned_pod_df.groupBy("pod_id").agg(count("Timestamp").alias("timestamp_count"))
         quality_filtered_pods = quality_filtered_pod_df.filter(col("timestamp_count") >= 2*time_steps)
 
-        #Processed pod DF                                                      
+        #Processed pod DF
         final_pod_df = cleaned_pod_df.filter(col("pod_id").isin(quality_filtered_pods["pod_id"]))
         final_pod_df = final_pod_df.sort("Timestamp")
-                
+
         #Drop duplicates on Pod_ID and Timestamp and keep first
         final_pod_df = final_pod_df.dropDuplicates(['pod_id', 'Timestamp'])
-        
-        #Drop rows with nans 
+
+        #Drop rows with nans
         final_pod_df = final_pod_df.na.drop("all")
-           
-        
+
+
         return features_df, final_pod_df
     else:
         empty_df = pd.DataFrame()
@@ -94,16 +94,16 @@ def pod_ad_feature_engineering(pod_id, input_df, input_features, input_scaled_fe
     ------
             pod_id: String
             randomly pick pod id
-            
+
             input_df: df
-            preprocessing and filtered pod df 
-            
+            preprocessing and filtered pod df
+
             input_features: list
             list of selected features
-            
+
             input_scaled_features: list
             list of tobe scaled features
-            
+
             input_time_steps: int
             model parameter time steps
 
@@ -111,7 +111,7 @@ def pod_ad_feature_engineering(pod_id, input_df, input_features, input_scaled_fe
     -------
             pod_fe_df: df
             training data df for exposing it as data product
-            
+
     """
 
     ##pick random df, and normalize
@@ -136,24 +136,24 @@ def pod_list_generator(input_data_type, input_split_ratio, input_pod_df, input_p
     ------
             input_data_type: String
             builds n_samples based on input string
-            
+
             input_split_ratio: list
             list of split parameters
-            
+
             input_pod_df: df
-            preprocessing and filtered pod df 
-            
+            preprocessing and filtered pod df
+
             input_pod_features_df: df
             processed pod features df
- 
+
     outputs
     -------
             pod_list: list
             randomly selected list of pod id's with replacement
-            
+
             input_pod_df: df
             final pod df with newly added columns
-            
+
     """
     model_parameters = input_pod_features_df["model_parameters"].iloc[0]
     time_steps = model_parameters["time_steps"]
@@ -166,9 +166,9 @@ def pod_list_generator(input_data_type, input_split_ratio, input_pod_df, input_p
 
     input_pod_df['freq'] = input_pod_df.groupby('pod_id')['pod_id'].transform('count')
     input_pod_df = input_pod_df[input_pod_df["freq"] > time_steps]
-    
+
     pod_list = input_pod_df['pod_id'].sample(n_samples).to_list()
-    
+
     return pod_list, input_pod_df
 
 
@@ -176,7 +176,7 @@ def pod_fe_pipeline(feature_group_name, feature_version,
             partition_year, partition_month, partition_day,
             partition_hour, spark_config_setup,
             bucket):
-    
+
     ##building file name dynamically
     if partition_hour == -1:
         file_name = f'{partition_year}_{partition_month}_{partition_day}'
@@ -184,10 +184,10 @@ def pod_fe_pipeline(feature_group_name, feature_version,
         file_name = f'{partition_year}_{partition_month}'
     else:
         file_name = f'{partition_year}_{partition_month}_{partition_day}_{partition_hour}'
-    
+
     #pre processing
     pod_features_data, pod_processed_data = pod_ad_preprocessing(feature_group_name, feature_version, partition_year, partition_month, partition_day, partition_hour, spark_config_setup)
-    
+
     #parsing model parameters
     scaled_features = []
     model_parameters = pod_features_data["model_parameters"].iloc[0]
@@ -219,12 +219,12 @@ def pod_fe_pipeline(feature_group_name, feature_version,
     #generating random selected list of pod id's
     selected_pod_train_list, processed_pod_train_data = pod_list_generator( 'train', [pod_train_split,pod_test_split], pod_train_data, pod_features_data)
     selected_pod_test_list, processed_pod_test_data = pod_list_generator( 'test', [pod_train_split,pod_test_split], pod_test_data, pod_features_data)
-    
+
     #getting number of cores per kernel
     num_cores = multiprocessing.cpu_count()
 
     #Train data feature engineering
-    pod_training_list = multiprocessing.Pool(num_cores).map(partial(pod_ad_feature_engineering, 
+    pod_training_list = multiprocessing.Pool(num_cores).map(partial(pod_ad_feature_engineering,
                          input_df=processed_pod_train_data, input_features=features, input_scaled_features=scaled_features, input_time_steps=time_steps), selected_pod_train_list)
     pod_training_df = pd.concat(pod_training_list)
     pod_training_tensor = np.array(list(map(lambda x: x.to_numpy(), pod_training_list)))
@@ -233,14 +233,13 @@ def pod_fe_pipeline(feature_group_name, feature_version,
     s3_utils.awswrangler_pandas_dataframe_to_s3(pod_training_df, "data" , "pandas_df", f'training_{file_name}.parquet')
 
     #Test data feature engineering
-    pod_testing_list = multiprocessing.Pool(num_cores).map(partial(pod_ad_feature_engineering, 
+    pod_testing_list = multiprocessing.Pool(num_cores).map(partial(pod_ad_feature_engineering,
                          input_df=processed_pod_test_data, input_features=features, input_scaled_features=scaled_features, input_time_steps=time_steps), selected_pod_test_list)
     pod_testing_df = pd.concat(pod_testing_list)
     pod_testing_tensor = np.array(list(map(lambda x: x.to_numpy(), pod_testing_list)))
     pod_testing_tensor = pod_testing_tensor[:,:,-len(scaled_features):]
     s3_utils.write_tensor(pod_testing_tensor, "data" , "tensors", f'testing_{file_name}.npy')
     s3_utils.awswrangler_pandas_dataframe_to_s3(pod_testing_df, "data" , "pandas_df", f'testing_{file_name}.parquet')
-  
 
-      
-    
+
+
--- eks_ml_pipeline/training.py
+++ eks_ml_pipeline/training.py
@@ -1,7 +1,7 @@
 """
-Train and save Anomaly Detection models with this single function.
+Streamlied train and save of Models for Encode Decode Residual Anomaly Scoring.
 
-Contributed by Evgeniya Dontsova and Vinayak Sharma
+Contributed by Evgeniya Dontsova, Vinayak Sharma, and David Cherney
 MSS Dish 5g - Pattern Detection
 """
 
@@ -9,128 +9,141 @@ import numpy as np
 import tf2onnx
 from .utilities import S3Utilities
 
-def model_training_pipeline(encode_decode_model,
-                            feature_group_name,
-                            feature_input_version,
-                            data_bucketname,
-                            train_data_filename,
-                            test_data_filename,
-                            save_model_local_path,
-                            model_bucketname,
-                            model_filename,
-                            upload_zip,
-                            upload_onnx,
-                            upload_npy,
-                            clean_local_folder
-                            ):
+class ModelTraining:
+    """A class for training models and storing results.
 
-    """
-    Generalized model training pipeline
-
-    inputs
+    Parameters
     ------
-    encode_decode_model: Class
-        initialized model class object that has
-        fit(), predict(), save_model(), load_model(),
-        and clean_model() methods
-
-    feature_group_name: str
-        json name to get the required features
-
-    feature_input_version: str
-        json version to get the latest features
-
-    data_bucketname: str
-        s3 bucket name where training data is saved
-
-    train_data_filename: str
-        filename where training data is saved
-
-    test_data_filename: str
-        filename where testing data is saved
-
-    save_model_local_path: str
-        local path to save trained model
-        (by default it is one level above git repo)
-
-    model_bucketname: str
-        s3 bucket name where trained model will be saved
-
-    model_filename: str
-        model filename where training model will be saved
-
-    upload_zip: bool
-        flag to save model in zip format
-
-    upload_onnx: bool
-        flag to save model in onnx format
-
-    upload_npy: bool
-        flag to save model in npy format
-
-    clean_local_folder: bool
-        flag to delete or keep locally saved model directory or files
+    training_inputs : List
+        Intended to be passed from the functions defined in the module
+        named training_input.py.
 
     outputs
     -------
     None
-
     """
+    def __init__(self, training_inputs):
+        # model and feature specification
+        self.encode_decode_model = training_inputs[0]
+        self.feature_group_name = training_inputs[1]
+        self.feature_input_version = training_inputs[2]
+        # data locations
+        self.data_bucketname = training_inputs[3]
+        self.train_data_filename = training_inputs[4]
+        self.test_data_filename = training_inputs[5]
+        # save model locations
+        self.save_model_local_path = training_inputs[6]
+        self.model_bucketname = training_inputs[7]
+        self.model_filename = training_inputs[8]
+        # other
+        self.model = None
+        self.s3_utilities = None
+        print(f'You are all set up to train a\n {self.encode_decode_model}'
+              +'\nusing the .train method.')
 
 
-    ###Initialize s3 utilities class
-    s3_utils = S3Utilities(bucket_name = data_bucketname,
-                           model_name = feature_group_name,
-                           version = feature_input_version)
+    def initialize_s3(self):
+        """Initialize s3 utilities class"""
+        self.s3_utilities = S3Utilities(bucket_name = self.data_bucketname,
+                               model_name = self.feature_group_name,
+                               version = self.feature_input_version)
+
+    def load_train_data(self):
+        """Load training data: read from s3 bucket"""
+        self.initialize_s3()
+        training_tensor = self.s3_utilities.read_tensor(folder = "data",
+            type_ = "tensors", file_name = self.train_data_filename)
+        return training_tensor
+
+    def ensure_np_type(self):
+        """Additional data cleaning: converting everything into np.float32"""
+        training_tensor = self.load_train_data()
+        training_tensor = np.asarray(training_tensor).astype(np.float32)
+        return training_tensor
+
+    def train(self):
+        """Train model on the object's training dataset.
+
+        Parameters
+        ----------
+            None
+
+        Returns
+        -------
+            None
+        """
+        training_tensor = self.ensure_np_type()
+        model = self.encode_decode_model.fit(training_tensor)
+        print('\nModel is trained.')
+        ###Save model
+        self.model = model
+        print('\nModel is saved in memory as the attribute .model.')
+        self.encode_decode_model.save_model(self.save_model_local_path)
+        print(f'\nModel is saved locally in {self.save_model_local_path}.')
+        print('\nTo save in S3 use the .save_to_s3 method.')
 
 
-    ###Load training data: read from s3 bucket
-    training_tensor = s3_utils.read_tensor(folder = "data",
-                                           type_ = "tensors",
-                                           file_name = train_data_filename)
+    def save_to_s3(self,
+            upload_zip=False, upload_onnx=False, upload_npy=False,
+            delete_local = True):
+        """Save a trained model object to s3 bucket.
 
-    #Additional data cleaning: converting everything into np.float32
-    training_tensor = np.asarray(training_tensor).astype(np.float32)
+        Parameters
+        ----------
+        upload_zip : Bool (optional, default=False)
+            If True than upload a .zip file to S3.
 
-    ###Train model
-    model = encode_decode_model.fit(training_tensor)
+        upload_onnx : Bool (optional, default=False)
+            If True than upload a .onyx file to S3.
 
-    ###Save model
-    encode_decode_model.save_model(save_model_local_path)
+        upload_npy : Bool (optional, default=False)
+            If True than upload a .npy file to S3.
 
-    ####Save model object to s3 bucket
-    #save zipped model object to s3 bucket
-    if upload_zip:
+        delete_local : Bool (optional, default=True)
+            If True than delete the locally saved model.
 
-        s3_utils.zip_and_upload(local_path = save_model_local_path,
-                                folder = "models",
-                                type_ = "zipped_models",
-                                file_name = model_filename + ".zip")
+        Returns
+        -------
+        None
+        """
 
-    #save onnx model object to s3 bucket
-    if upload_onnx:
+        # Check that model exists and thus can be saved.
+        if self.model is None:
+            print('A model must be trained before it is saved.')
+            return
 
-        save_model_local_path_onnx = (save_model_local_path + '/'
-                                      + model_filename + ".onnx")
-        #Save model locally in .onnx format
-        tf2onnx.convert.from_keras(
-            encode_decode_model.nn, output_path = save_model_local_path_onnx)
-        s3_utils.upload_file(local_path = save_model_local_path_onnx,
-                             bucket_name = model_bucketname,
-                             key = '/'.join([feature_group_name,
-                                             feature_input_version,
-                                             "models", "onnx_models",
-                                             model_filename + ".onnx"]
-                                           )
-                            )
+        if upload_zip:
+            #save zipped model object to s3 bucket
+            self.s3_utilities.zip_and_upload(local_path = self.save_model_local_path,
+                                    folder = "models",
+                                    type_ = "zipped_models",
+                                    file_name = self.model_filename + ".zip")
+        if upload_onnx:
+            # Save onnx model object to s3 bucket.
+            save_model_local_path_onnx = (self.save_model_local_path + '/'
+                                          + self.model_filename + ".onnx")
+            # Save model locally in .onnx format.
+            tf2onnx.convert.from_keras(
+                self.encode_decode_model.nn,
+                output_path = save_model_local_path_onnx)
+            self.s3_utilities.upload_file(
+                local_path = save_model_local_path_onnx,
+                bucket_name = self.model_bucketname,
+                key = '/'.join([self.feature_group_name,
+                                 self.feature_input_version,
+                                 "models", "onnx_models",
+                                 self.model_filename + ".onnx"]
+                               )
+                                )
 
-    #save npy model object to s3 bucket
-    if upload_npy:
-        s3_utils.write_tensor(tensor = model,
-                              folder = "models",
-                              type_ = "npy_models",
-                              file_name = model_filename + ".npy")
+        if upload_npy:
+            # Save npy model object to s3 bucket.
+            self.s3_utilities.write_tensor(tensor = self.model,
+                                  folder = "models",
+                                  type_ = "npy_models",
+                                  file_name = self.model_filename + ".npy")
+            print('\nModel uploaded to S3 as .npy type.')
 
-    #Delete locally saved model
-    if clean_local_folder:
-        encode_decode_model.clean_model(save_model_local_path)
+        if delete_local:
+            # Delete locally saved model
+            self.encode_decode_model.clean_model(self.save_model_local_path)
