3d35d0b0f81b71ef4f6fda8e11772499cb6040dd
Vinayak Sharma <vinnyshark0@gmail.com>
2023-02-06 15:59:49 -0700

    adding bucket_name from github secrets to the pytests

--- tests/test_pipeline_eval.py
+++ tests/test_pipeline_eval.py
@@ -8,16 +8,16 @@ def test_aePipelineEval(aeTestInput):
     ttp_ae.test()
     
     errors = []
-    
+    Bucket_Name = os.environ.get("BUCKET_NAME_PYTEST")
     try:
-        preds_file_head = s3.head_object(Bucket='dish-5g.core.pd.g.dp.eks.logs.e', Key='pytest_autoencoder_ad/v0.0.1/models/predictions/aeDummyDataTest_predictions.npy')
-        s3.delete_object(Bucket='dish-5g.core.pd.g.dp.eks.logs.e', Key='pytest_autoencoder_ad/v0.0.1/models/predictions/aeDummyDataTest_predictions.npy')
+        preds_file_head = s3.head_object(Bucket=Bucket_Name, Key='pytest_autoencoder_ad/v0.0.1/models/predictions/aeDummyDataTest_predictions.npy')
+        s3.delete_object(Bucket=Bucket_Name, Key='pytest_autoencoder_ad/v0.0.1/models/predictions/aeDummyDataTest_predictions.npy')
     except Exception as e:
         errors.append(e)
         
     try:   
-        res_file_head = s3.head_object(Bucket='dish-5g.core.pd.g.dp.eks.logs.e', Key='pytest_autoencoder_ad/v0.0.1/models/predictions/aeDummyDataTest_residuals.npy')
-        s3.delete_object(Bucket='dish-5g.core.pd.g.dp.eks.logs.e', Key='pytest_autoencoder_ad/v0.0.1/models/predictions/aeDummyDataTest_residuals.npy')
+        res_file_head = s3.head_object(Bucket=Bucket_Name, Key='pytest_autoencoder_ad/v0.0.1/models/predictions/aeDummyDataTest_residuals.npy')
+        s3.delete_object(Bucket=Bucket_Name, Key='pytest_autoencoder_ad/v0.0.1/models/predictions/aeDummyDataTest_residuals.npy')
     except Exception as e:
         errors.append(e)
         
@@ -33,18 +33,18 @@ def test_pcaPipelineEval(pcaTestInput):
     s3 = boto3.client('s3')
     ttp_pca = TrainTestPipelines(pcaTestInput)
     ttp_pca.test()
-    
+    Bucket_Name = os.environ.get("BUCKET_NAME_PYTEST")
     errors = []
     
     try:
-        preds_file_head = s3.head_object(Bucket='dish-5g.core.pd.g.dp.eks.logs.e', Key='pytest_pca_ad/v0.0.1/models/predictions/pcaDummyDataTest_predictions.npy')
+        preds_file_head = s3.head_object(Bucket=Bucket_Name, Key='pytest_pca_ad/v0.0.1/models/predictions/pcaDummyDataTest_predictions.npy')
         s3.delete_object(Bucket='dish-5g.core.pd.g.dp.eks.logs.e', Key='pytest_pca_ad/v0.0.1/models/predictions/pcaDummyDataTest_predictions.npy')
     except Exception as e:
         errors.append(e)
         
     try:   
-        res_file_head = s3.head_object(Bucket='dish-5g.core.pd.g.dp.eks.logs.e', Key='pytest_pca_ad/v0.0.1/models/predictions/pcaDummyDataTest_residuals.npy')
-        s3.delete_object(Bucket='dish-5g.core.pd.g.dp.eks.logs.e', Key='pytest_pca_ad/v0.0.1/models/predictions/pcaDummyDataTest_residuals.npy')
+        res_file_head = s3.head_object(Bucket= Bucket_Name, Key='pytest_pca_ad/v0.0.1/models/predictions/pcaDummyDataTest_residuals.npy')
+        s3.delete_object(Bucket= Bucket_Name, Key='pytest_pca_ad/v0.0.1/models/predictions/pcaDummyDataTest_residuals.npy')
     except Exception as e:
         errors.append(e)
         
--- tests/test_pipeline_training.py
+++ tests/test_pipeline_training.py
@@ -7,16 +7,16 @@ def test_aePipelineTraining(aeTrainInput):
     ttp_ae.train()
     
     errors = []
-    
+    Bucket_Name = os.environ.get("BUCKET_NAME_PYTEST")
     try:
-        zip_file_head = s3.head_object(Bucket='dish-5g.core.pd.g.dp.eks.logs.e', Key='pytest_autoencoder_ad/v0.0.1/models/zipped_models/train_autoencoder_ad_model_v0.0.1_aeDummyDataTrain.zip')
-        s3.delete_object(Bucket='dish-5g.core.pd.g.dp.eks.logs.e', Key='pytest_autoencoder_ad/v0.0.1/models/zipped_models/train_autoencoder_ad_model_v0.0.1_aeDummyDataTrain.zip')
+        zip_file_head = s3.head_object(Bucket=Bucket_Name, Key='pytest_autoencoder_ad/v0.0.1/models/zipped_models/train_autoencoder_ad_model_v0.0.1_aeDummyDataTrain.zip')
+        s3.delete_object(Bucket=Bucket_Name, Key='pytest_autoencoder_ad/v0.0.1/models/zipped_models/train_autoencoder_ad_model_v0.0.1_aeDummyDataTrain.zip')
     except Exception as e:
         errors.append(e)
         
     try:   
-        onnx_file_head = s3.head_object(Bucket='dish-5g.core.pd.g.dp.eks.logs.e', Key='pytest_autoencoder_ad/v0.0.1/models/onnx_models/train_autoencoder_ad_model_v0.0.1_aeDummyDataTrain.onnx')
-        s3.delete_object(Bucket='dish-5g.core.pd.g.dp.eks.logs.e', Key='pytest_autoencoder_ad/v0.0.1/models/onnx_models/train_autoencoder_ad_model_v0.0.1_aeDummyDataTrain.onnx')
+        onnx_file_head = s3.head_object(Bucket=Bucket_Name, Key='pytest_autoencoder_ad/v0.0.1/models/onnx_models/train_autoencoder_ad_model_v0.0.1_aeDummyDataTrain.onnx')
+        s3.delete_object(Bucket=Bucket_Name, Key='pytest_autoencoder_ad/v0.0.1/models/onnx_models/train_autoencoder_ad_model_v0.0.1_aeDummyDataTrain.onnx')
     except Exception as e:
         errors.append(e)
         
@@ -30,12 +30,12 @@ def test_pcaPipelineTraining(pcaTrainInput):
     s3 = boto3.client('s3')
     ttp_pca = TrainTestPipelines(pcaTrainInput)
     ttp_pca.train()
-    
+    Bucket_Name = os.environ.get("BUCKET_NAME_PYTEST")
     errors = []
     
     try:
-        zip_file_head = s3.head_object(Bucket='dish-5g.core.pd.g.dp.eks.logs.e', Key='pytest_pca_ad/v0.0.1/models/npy_models/train_pca_ad_model_v0.0.1_pcaDummyDataTrain.npy')
-        s3.delete_object(Bucket='dish-5g.core.pd.g.dp.eks.logs.e', Key='pytest_pca_ad/v0.0.1/models/npy_models/train_pca_ad_model_v0.0.1_pcaDummyDataTrain.npy')
+        zip_file_head = s3.head_object(Bucket=Bucket_Name, Key='pytest_pca_ad/v0.0.1/models/npy_models/train_pca_ad_model_v0.0.1_pcaDummyDataTrain.npy')
+        s3.delete_object(Bucket=Bucket_Name, Key='pytest_pca_ad/v0.0.1/models/npy_models/train_pca_ad_model_v0.0.1_pcaDummyDataTrain.npy')
     except Exception as e:
         errors.append(e)
         
