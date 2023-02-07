d964af72c6a595e24bfc58e9bbc95b1dbf2cd32a
Vinayak Sharma <vinnyshark0@gmail.com>
2023-02-07 13:45:42 -0700

    used black to reformat up to pep8 standards

--- tests/test_pipeline_eval.py
+++ tests/test_pipeline_eval.py
@@ -2,42 +2,48 @@ from eks_ml_pipeline import TrainTestPipelines
 import boto3
 import os
 
-def test_ae_pipeline_eval(ae_test_input,bucket_name):
+
+def test_ae_pipeline_eval(ae_test_input, bucket_name):
     """
     This testing module verifies the evaluation logic for the autoencoder evaluation pipeline. It checks if a model is read from the correct s3 path after training.
     And it also checks if the pipeline is able to read our presaved evaluation tensor from s3. 
     Then the test checks if the residuals and predictions are stored in the correct s3 path. 
-
-     Inputs: fixtures of training input and bucket name. 
-     Output: None
+    
+    Inputs: fixtures of training input and bucket name. 
+    Output: None
     """
-    s3 = boto3.client('s3')
+    s3 = boto3.client("s3")
     ttp_ae = TrainTestPipelines(ae_test_input)
     ttp_ae.test()
-    
+
     errors = []
 
     try:
-        preds_file_head = s3.head_object(Bucket=bucket_name, Key='pytest_autoencoder_ad/v0.0.1/models/predictions/aeDummyDataTest_predictions.npy')
-        s3.delete_object(Bucket=bucket_name, Key='pytest_autoencoder_ad/v0.0.1/models/predictions/aeDummyDataTest_predictions.npy')
+        pred_key = "pytest_autoencoder_ad/v0.0.1/models/predictions/aeDummyDataTest_predictions.npy"
+        preds_file_head = s3.head_object(Bucket=bucket_name, Key=pred_key)
+        s3.delete_object(Bucket=bucket_name, Key=pred_key)
     except Exception as e:
         errors.append(e)
-        
-    try:   
-        res_file_head = s3.head_object(Bucket=bucket_name, Key='pytest_autoencoder_ad/v0.0.1/models/predictions/aeDummyDataTest_residuals.npy')
-        s3.delete_object(Bucket=bucket_name, Key='pytest_autoencoder_ad/v0.0.1/models/predictions/aeDummyDataTest_residuals.npy')
+
+    try:
+        res_file_head = s3.head_object(
+            Bucket=bucket_name,
+            Key="pytest_autoencoder_ad/v0.0.1/models/predictions/aeDummyDataTest_residuals.npy",
+        )
+        s3.delete_object(
+            Bucket=bucket_name,
+            Key="pytest_autoencoder_ad/v0.0.1/models/predictions/aeDummyDataTest_residuals.npy",
+        )
     except Exception as e:
         errors.append(e)
-        
-    if(os.path.exists("../test_autoencoder") == True):
+
+    if os.path.exists("../test_autoencoder") == True:
         errors.append("AE Model not deleted correctly")
-    
-        
-    assert(len(errors)==0)
-    
-    
-    
-def test_pca_pipeline_eval(pca_test_input,bucket_name):
+
+    assert len(errors) == 0
+
+
+def test_pca_pipeline_eval(pca_test_input, bucket_name):
     """
     This testing module verifies the evaluation logic for the pca evaluation pipeline. It checks if a model is read from the correct s3 path after training.
     And it also checks if the pipeline is able to read our presaved evaluation tensor from s3. 
@@ -46,28 +52,36 @@ def test_pca_pipeline_eval(pca_test_input,bucket_name):
      Inputs: fixtures of training input and bucket name. 
      Output: None
     """
-    s3 = boto3.client('s3')
+    s3 = boto3.client("s3")
     ttp_pca = TrainTestPipelines(pca_test_input)
     ttp_pca.test()
     errors = []
-    
+
     try:
-        preds_file_head = s3.head_object(Bucket=bucket_name, Key='pytest_pca_ad/v0.0.1/models/predictions/pcaDummyDataTest_predictions.npy')
-        s3.delete_object(Bucket=bucket_name, Key='pytest_pca_ad/v0.0.1/models/predictions/pcaDummyDataTest_predictions.npy')
+        preds_file_head = s3.head_object(
+            Bucket=bucket_name,
+            Key="pytest_pca_ad/v0.0.1/models/predictions/pcaDummyDataTest_predictions.npy",
+        )
+        s3.delete_object(
+            Bucket=bucket_name,
+            Key="pytest_pca_ad/v0.0.1/models/predictions/pcaDummyDataTest_predictions.npy",
+        )
     except Exception as e:
         errors.append(e)
-        
-    try:   
-        res_file_head = s3.head_object(Bucket= bucket_name, Key='pytest_pca_ad/v0.0.1/models/predictions/pcaDummyDataTest_residuals.npy')
-        s3.delete_object(Bucket= bucket_name, Key='pytest_pca_ad/v0.0.1/models/predictions/pcaDummyDataTest_residuals.npy')
+
+    try:
+        res_file_head = s3.head_object(
+            Bucket=bucket_name,
+            Key="pytest_pca_ad/v0.0.1/models/predictions/pcaDummyDataTest_residuals.npy",
+        )
+        s3.delete_object(
+            Bucket=bucket_name,
+            Key="pytest_pca_ad/v0.0.1/models/predictions/pcaDummyDataTest_residuals.npy",
+        )
     except Exception as e:
         errors.append(e)
-        
-    if(os.path.exists("../test_pca.npy") == True):
+
+    if os.path.exists("../test_pca.npy") == True:
         errors.append("AE Model not deleted correctly")
-        
-        
-    assert(len(errors)==0)
 
-
-
+    assert len(errors) == 0
--- tests/test_pipeline_training.py
+++ tests/test_pipeline_training.py
@@ -2,7 +2,7 @@ from eks_ml_pipeline import TrainTestPipelines
 import boto3
 
 
-def test_ae_pipeline_training(ae_train_input,bucket_name):
+def test_ae_pipeline_training(ae_train_input, bucket_name):
     """
     This testing module verifies the training logic for the autoencoder training pipeline. It checks if a model is saved to the correct s3 path after training.
     And it also checks if the pipeline is able to read our presaved training tensor from s3. 
@@ -10,30 +10,39 @@ def test_ae_pipeline_training(ae_train_input,bucket_name):
      Inputs: fixtures of training input and bucket name. 
      Output: None
     """
-    s3 = boto3.client('s3')
+    s3 = boto3.client("s3")
     ttp_ae = TrainTestPipelines(ae_train_input)
     ttp_ae.train()
-    
+
     errors = []
     try:
-        zip_file_head = s3.head_object(Bucket=bucket_name, Key='pytest_autoencoder_ad/v0.0.1/models/zipped_models/train_autoencoder_ad_model_v0.0.1_aeDummyDataTrain.zip')
-        s3.delete_object(Bucket=bucket_name, Key='pytest_autoencoder_ad/v0.0.1/models/zipped_models/train_autoencoder_ad_model_v0.0.1_aeDummyDataTrain.zip')
+        zip_file_head = s3.head_object(
+            Bucket=bucket_name,
+            Key="pytest_autoencoder_ad/v0.0.1/models/zipped_models/train_autoencoder_ad_model_v0.0.1_aeDummyDataTrain.zip",
+        )
+        s3.delete_object(
+            Bucket=bucket_name,
+            Key="pytest_autoencoder_ad/v0.0.1/models/zipped_models/train_autoencoder_ad_model_v0.0.1_aeDummyDataTrain.zip",
+        )
     except Exception as e:
         errors.append(e)
-        
-    try:   
-        onnx_file_head = s3.head_object(Bucket=bucket_name, Key='pytest_autoencoder_ad/v0.0.1/models/onnx_models/train_autoencoder_ad_model_v0.0.1_aeDummyDataTrain.onnx')
-        s3.delete_object(Bucket=bucket_name, Key='pytest_autoencoder_ad/v0.0.1/models/onnx_models/train_autoencoder_ad_model_v0.0.1_aeDummyDataTrain.onnx')
+
+    try:
+        onnx_file_head = s3.head_object(
+            Bucket=bucket_name,
+            Key="pytest_autoencoder_ad/v0.0.1/models/onnx_models/train_autoencoder_ad_model_v0.0.1_aeDummyDataTrain.onnx",
+        )
+        s3.delete_object(
+            Bucket=bucket_name,
+            Key="pytest_autoencoder_ad/v0.0.1/models/onnx_models/train_autoencoder_ad_model_v0.0.1_aeDummyDataTrain.onnx",
+        )
     except Exception as e:
         errors.append(e)
-        
-    
-        
-    assert(len(errors)==0)
-    
-    
-    
-def test_pca_pipeline_training(pca_train_input,bucket_name):
+
+    assert len(errors) == 0
+
+
+def test_pca_pipeline_training(pca_train_input, bucket_name):
     """
     This testing module verifies the training logic for the pca training pipeline. It checks if a model is saved to the correct s3 path after training.
     And it also checks if the pipeline is able to read our presaved training tensor from s3. 
@@ -41,17 +50,21 @@ def test_pca_pipeline_training(pca_train_input,bucket_name):
      Inputs: fixtures of training input and bucket name 
      Output: None
     """
-    s3 = boto3.client('s3')
+    s3 = boto3.client("s3")
     ttp_pca = TrainTestPipelines(pca_train_input)
     ttp_pca.train()
     errors = []
-    
+
     try:
-        zip_file_head = s3.head_object(Bucket=bucket_name, Key='pytest_pca_ad/v0.0.1/models/npy_models/train_pca_ad_model_v0.0.1_pcaDummyDataTrain.npy')
-        s3.delete_object(Bucket=bucket_name, Key='pytest_pca_ad/v0.0.1/models/npy_models/train_pca_ad_model_v0.0.1_pcaDummyDataTrain.npy')
+        zip_file_head = s3.head_object(
+            Bucket=bucket_name,
+            Key="pytest_pca_ad/v0.0.1/models/npy_models/train_pca_ad_model_v0.0.1_pcaDummyDataTrain.npy",
+        )
+        s3.delete_object(
+            Bucket=bucket_name,
+            Key="pytest_pca_ad/v0.0.1/models/npy_models/train_pca_ad_model_v0.0.1_pcaDummyDataTrain.npy",
+        )
     except Exception as e:
         errors.append(e)
-        
-    assert(len(errors)==0)
 
-    
+    assert len(errors) == 0
