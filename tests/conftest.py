203d543fdee4094343cbaa732eb6340a056b8889
Vinayak Sharma <vinnyshark0@gmail.com>
2023-02-07 13:45:42 -0700

    used black to reformat up to pep8 standards

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,11 +12,18 @@ import os
 # functions to mark slow tests and skip them.
 def pytest_addoption(parser):
     parser.addoption(
-        "--slow", action="store_true", default=False, help="run (slow) performance tests"
+        "--slow",
+        action="store_true",
+        default=False,
+        help="run (slow) performance tests",
     )
 
+
 def pytest_configure(config):
-    config.addinivalue_line("markers", "slow: mark test as a (potentially slow) performance test")
+    config.addinivalue_line(
+        "markers", "slow: mark test as a (potentially slow) performance test"
+    )
+
 
 def pytest_collection_modifyitems(config, items):
     if config.getoption("--slow"):
@@ -27,15 +34,16 @@ def pytest_collection_modifyitems(config, items):
             item.add_marker(skip_perf)
 
 
-@pytest.fixture(scope="module")           
-def bucket_name(): 
+@pytest.fixture(scope="module")
+def bucket_name():
     """
     get bucket name from the github workflow runner secrets
-    """  
+    """
     BUCKET_NAME = os.environ.get("BUCKET_NAME_PYTEST")
     return BUCKET_NAME
 
-@pytest.fixture(scope="module")           
+
+@pytest.fixture(scope="module")
 def ae_train_input(bucket_name):
     """
     This fixture creates inputs to train the desired model. It includes all of bucket versioning and model versioning needed 
@@ -52,11 +60,11 @@ def ae_train_input(bucket_name):
         training pipeline
     """
 
-    #*****************************************************#
-    #********** data and model input parameters **********#
-    #*****************************************************#
+    # *****************************************************#
+    # ********** data and model input parameters **********#
+    # *****************************************************#
 
-    #feature_selection
+    # feature_selection
     feature_group_name = "pytest_autoencoder_ad"
     feature_input_version = "v0.0.1"
 
@@ -68,30 +76,32 @@ def ae_train_input(bucket_name):
     # save_model_locations
     save_model_local_path = "../test_autoencoder"
     model_bucketname = bucket_name
-    model_name = 'train_autoencoder_ad'
+    model_name = "train_autoencoder_ad"
     model_version = "v0.0.1"
 
-    #Define model filename and path
-    model_filename = '_'.join(
-        [model_name,
-        "model",
-        model_version,
-        train_data_filename.split(".")[-2] # all preceeding extension
-        ])
+    # Define model filename and path
+    model_filename = "_".join(
+        [
+            model_name,
+            "model",
+            model_version,
+            train_data_filename.split(".")[-2],  # all preceeding extension
+        ]
+    )
 
-    #********************************************#
-    #********** initialize model class **********#
-    #********************************************#
+    # ********************************************#
+    # ********** initialize model class **********#
+    # ********************************************#
 
     features_df = get_features(feature_group_name, feature_input_version)
     model_parameters = features_df["model_parameters"].iloc[0]
 
-    #Initialize autoencoder model class with specific parameters
+    # Initialize autoencoder model class with specific parameters
     encode_decode_model = AutoencoderModelDish5g(
-        time_steps = model_parameters["time_steps"],
-        batch_size = model_parameters["batch_size"],
-        epochs=1
-        )
+        time_steps=model_parameters["time_steps"],
+        batch_size=model_parameters["batch_size"],
+        epochs=1,
+    )
 
     # File flags
     upload_zip = True
@@ -99,15 +109,20 @@ def ae_train_input(bucket_name):
     upload_npy = False
     clean_local_folder = True
 
-    return [encode_decode_model,
-            [feature_group_name,feature_input_version], # feature_selection,
-            [data_bucketname, train_data_filename, test_data_filename], # data_locations
-            [save_model_local_path, model_bucketname, model_filename], # save_model_locations,
-            [upload_zip, upload_onnx, upload_npy, clean_local_folder] # file_flags
-           ]
-            
+    return [
+        encode_decode_model,
+        [feature_group_name, feature_input_version],  # feature_selection,
+        [data_bucketname, train_data_filename, test_data_filename],  # data_locations
+        [
+            save_model_local_path,
+            model_bucketname,
+            model_filename,
+        ],  # save_model_locations,
+        [upload_zip, upload_onnx, upload_npy, clean_local_folder],  # file_flags
+    ]
 
-@pytest.fixture(scope="module")  
+
+@pytest.fixture(scope="module")
 def ae_test_input(bucket_name):
     """
     This fixture creates inputs to evaluate the desired model. It includes all of bucket versioning and model versioning needed 
@@ -124,11 +139,11 @@ def ae_test_input(bucket_name):
         training pipeline
     """
 
-    #*****************************************************#
-    #********** data and model input parameters **********#
-    #*****************************************************#
+    # *****************************************************#
+    # ********** data and model input parameters **********#
+    # *****************************************************#
 
-    #feature_selection
+    # feature_selection
     feature_group_name = "pytest_autoencoder_ad"
     feature_input_version = "v0.0.1"
 
@@ -140,30 +155,32 @@ def ae_test_input(bucket_name):
     # save_model_locations
     save_model_local_path = "../test_autoencoder"
     model_bucketname = bucket_name
-    model_name = 'test_autoencoder_ad'
+    model_name = "test_autoencoder_ad"
     model_version = "v0.0.1"
 
-    #Define model filename and path
-    model_filename = '_'.join(
-        [model_name,
-        "model",
-        model_version,
-        train_data_filename.split(".")[-2] # all preceeding extension
-        ])
+    # Define model filename and path
+    model_filename = "_".join(
+        [
+            model_name,
+            "model",
+            model_version,
+            train_data_filename.split(".")[-2],  # all preceeding extension
+        ]
+    )
 
-    #********************************************#
-    #********** initialize model class **********#
-    #********************************************#
+    # ********************************************#
+    # ********** initialize model class **********#
+    # ********************************************#
 
     features_df = get_features(feature_group_name, feature_input_version)
     model_parameters = features_df["model_parameters"].iloc[0]
 
-    #Initialize autoencoder model class with specific parameters
+    # Initialize autoencoder model class with specific parameters
     encode_decode_model = AutoencoderModelDish5g(
-        time_steps = model_parameters["time_steps"],
-        batch_size = model_parameters["batch_size"],
-        epochs=1
-        )
+        time_steps=model_parameters["time_steps"],
+        batch_size=model_parameters["batch_size"],
+        epochs=1,
+    )
 
     # File flags
     upload_zip = True
@@ -171,15 +188,20 @@ def ae_test_input(bucket_name):
     upload_npy = False
     clean_local_folder = True
 
-    return [encode_decode_model,
-            [feature_group_name,feature_input_version], # feature_selection,
-            [data_bucketname, train_data_filename, test_data_filename], # data_locations
-            [save_model_local_path, model_bucketname, model_filename], # save_model_locations,
-            [upload_zip, upload_onnx, upload_npy, clean_local_folder] # file_flags
-           ]
+    return [
+        encode_decode_model,
+        [feature_group_name, feature_input_version],  # feature_selection,
+        [data_bucketname, train_data_filename, test_data_filename],  # data_locations
+        [
+            save_model_local_path,
+            model_bucketname,
+            model_filename,
+        ],  # save_model_locations,
+        [upload_zip, upload_onnx, upload_npy, clean_local_folder],  # file_flags
+    ]
 
 
-@pytest.fixture(scope="module")           
+@pytest.fixture(scope="module")
 def pca_train_input(bucket_name):
     """
     This fixture creates inputs to train the desired model. It includes all of bucket versioning and model versioning needed 
@@ -195,9 +217,9 @@ def pca_train_input(bucket_name):
         training pipeline
     """
 
-    #*****************************************************#
-    #********** data and model input parameters **********#
-    #*****************************************************#
+    # *****************************************************#
+    # ********** data and model input parameters **********#
+    # *****************************************************#
 
     ##generate pipeline input params for pca
 
@@ -215,26 +237,29 @@ def pca_train_input(bucket_name):
     model_bucketname = bucket_name
     model_name = "train_pca_ad"
     model_version = "v0.0.1"
-    #Define model filename
-    model_filename = '_'.join(
-        [model_name, "model",
-         model_version,
-         train_data_filename.split(".")[-2] # All preceeding extension.
-        ])
+    # Define model filename
+    model_filename = "_".join(
+        [
+            model_name,
+            "model",
+            model_version,
+            train_data_filename.split(".")[-2],  # All preceeding extension.
+        ]
+    )
 
-    #********************************************#
-    #********** initialize model class **********#
-    #********************************************#
+    # ********************************************#
+    # ********** initialize model class **********#
+    # ********************************************#
 
     features_df = get_features(feature_group_name, feature_input_version)
     features = features_df["feature_name"].to_list()
     model_parameters = features_df["model_parameters"].iloc[0]
 
-    #Initialize pca model
+    # Initialize pca model
     encode_decode_model = PcaModelDish5g(
-        num_of_features = len(features),
-        timesteps_per_slice = model_parameters["time_steps"]
-        )
+        num_of_features=len(features),
+        timesteps_per_slice=model_parameters["time_steps"],
+    )
 
     # File flags
     upload_zip = False
@@ -242,15 +267,20 @@ def pca_train_input(bucket_name):
     upload_npy = True
     clean_local_folder = True
 
-    return [encode_decode_model,
-            [feature_group_name,feature_input_version], # feature_selection,
-            [data_bucketname, train_data_filename, test_data_filename], # data_locations
-            [save_model_local_path, model_bucketname, model_filename], # save_model_locations,
-            [upload_zip, upload_onnx, upload_npy, clean_local_folder] # file_flags
-           ]
+    return [
+        encode_decode_model,
+        [feature_group_name, feature_input_version],  # feature_selection,
+        [data_bucketname, train_data_filename, test_data_filename],  # data_locations
+        [
+            save_model_local_path,
+            model_bucketname,
+            model_filename,
+        ],  # save_model_locations,
+        [upload_zip, upload_onnx, upload_npy, clean_local_folder],  # file_flags
+    ]
 
 
-@pytest.fixture(scope="module")           
+@pytest.fixture(scope="module")
 def pca_test_input(bucket_name):
     """
     This fixture creates inputs to evaluate the desired model. It includes all of bucket versioning and model versioning needed 
@@ -266,9 +296,9 @@ def pca_test_input(bucket_name):
         training pipeline
     """
 
-    #*****************************************************#
-    #********** data and model input parameters **********#
-    #*****************************************************#
+    # *****************************************************#
+    # ********** data and model input parameters **********#
+    # *****************************************************#
 
     ##generate pipeline input params for pca
 
@@ -286,26 +316,29 @@ def pca_test_input(bucket_name):
     model_bucketname = bucket_name
     model_name = "test_pca_ad"
     model_version = "v0.0.1"
-    #Define model filename
-    model_filename = '_'.join(
-        [model_name, "model",
-         model_version,
-         train_data_filename.split(".")[-2] # All preceeding extension.
-        ])
+    # Define model filename
+    model_filename = "_".join(
+        [
+            model_name,
+            "model",
+            model_version,
+            train_data_filename.split(".")[-2],  # All preceeding extension.
+        ]
+    )
 
-    #********************************************#
-    #********** initialize model class **********#
-    #********************************************#
+    # ********************************************#
+    # ********** initialize model class **********#
+    # ********************************************#
 
     features_df = get_features(feature_group_name, feature_input_version)
     features = features_df["feature_name"].to_list()
     model_parameters = features_df["model_parameters"].iloc[0]
 
-    #Initialize pca model
+    # Initialize pca model
     encode_decode_model = PcaModelDish5g(
-        num_of_features = len(features),
-        timesteps_per_slice = model_parameters["time_steps"]
-        )
+        num_of_features=len(features),
+        timesteps_per_slice=model_parameters["time_steps"],
+    )
 
     # File flags
     upload_zip = False
@@ -313,9 +346,14 @@ def pca_test_input(bucket_name):
     upload_npy = True
     clean_local_folder = True
 
-    return [encode_decode_model,
-            [feature_group_name,feature_input_version], # feature_selection,
-            [data_bucketname, train_data_filename, test_data_filename], # data_locations
-            [save_model_local_path, model_bucketname, model_filename], # save_model_locations,
-            [upload_zip, upload_onnx, upload_npy, clean_local_folder] # file_flags
-           ]
+    return [
+        encode_decode_model,
+        [feature_group_name, feature_input_version],  # feature_selection,
+        [data_bucketname, train_data_filename, test_data_filename],  # data_locations
+        [
+            save_model_local_path,
+            model_bucketname,
+            model_filename,
+        ],  # save_model_locations,
+        [upload_zip, upload_onnx, upload_npy, clean_local_folder],  # file_flags
+    ]
