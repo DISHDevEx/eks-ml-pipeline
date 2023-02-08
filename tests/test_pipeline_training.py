423c7996752085e1b726c27e458ea2c79ef61ab1
Vinayak Sharma <vinnyshark0@gmail.com>
2023-02-08 09:58:06 -0700

    added pylint compliance. Broke rules where necessary

--- tests/test_pipeline_training.py
+++ tests/test_pipeline_training.py
@@ -1,32 +1,32 @@
-from eks_ml_pipeline import TrainTestPipelines
-import boto3
 """
 Module to contain all tests pertaining to pipelines that train models
 """
+from eks_ml_pipeline import TrainTestPipelines
+import boto3
 
 
 def test_ae_pipeline_training(ae_train_input, bucket_name):
     """
-    Verifies the training logic for the autoencoder training pipeline. 
+    Verifies the training logic for the autoencoder training pipeline.
     Checks if a model is saved to the correct s3 path after training.
-    Checks if the pipeline is able to read our presaved training tensor from s3. 
+    Checks if the pipeline is able to read our presaved training tensor from s3.
 
-     Inputs: fixtures of training input and bucket name. 
+     Inputs: fixtures of training input and bucket name.
      Output: None
 
      multiple try catch added to save time, train() can take 45seconds+
     """
-    s3 = boto3.client("s3")
+    s3_client = boto3.client("s3")
     ttp_ae = TrainTestPipelines(ae_train_input)
     ttp_ae.train()
 
     errors = []
     try:
-        zip_file_head = s3.head_object(
+        s3_client.head_object(
             Bucket=bucket_name,
             Key="pytest_autoencoder_ad/v0.0.1/models/zipped_models/train_autoencoder_ad_model_v0.0.1_aeDummyDataTrain.zip",
         )
-        s3.delete_object(
+        s3_client.delete_object(
             Bucket=bucket_name,
             Key="pytest_autoencoder_ad/v0.0.1/models/zipped_models/train_autoencoder_ad_model_v0.0.1_aeDummyDataTrain.zip",
         )
@@ -34,11 +34,11 @@ def test_ae_pipeline_training(ae_train_input, bucket_name):
         errors.append(error_message)
 
     try:
-        onnx_file_head = s3.head_object(
+        s3_client.head_object(
             Bucket=bucket_name,
             Key="pytest_autoencoder_ad/v0.0.1/models/onnx_models/train_autoencoder_ad_model_v0.0.1_aeDummyDataTrain.onnx",
         )
-        s3.delete_object(
+        s3_client.delete_object(
             Bucket=bucket_name,
             Key="pytest_autoencoder_ad/v0.0.1/models/onnx_models/train_autoencoder_ad_model_v0.0.1_aeDummyDataTrain.onnx",
         )
@@ -50,30 +50,30 @@ def test_ae_pipeline_training(ae_train_input, bucket_name):
 
 def test_pca_pipeline_training(pca_train_input, bucket_name):
     """
-    Verifies the training logic for the pca training pipeline. 
+    Verifies the training logic for the pca training pipeline.
     Checks if a model is saved to the correct s3 path after training.
-    Checks if the pipeline is able to read our presaved training tensor from s3. 
+    Checks if the pipeline is able to read our presaved training tensor from s3.
 
-     Inputs: fixtures of training input and bucket name 
+     Inputs: fixtures of training input and bucket name
      Output: None
 
      multiple try catch added to save time, train() can take 45seconds+
     """
-    s3 = boto3.client("s3")
+    s3_client = boto3.client("s3")
     ttp_pca = TrainTestPipelines(pca_train_input)
     ttp_pca.train()
     errors = []
 
     try:
-        zip_file_head = s3.head_object(
+        s3_client.head_object(
             Bucket=bucket_name,
             Key="pytest_pca_ad/v0.0.1/models/npy_models/train_pca_ad_model_v0.0.1_pcaDummyDataTrain.npy",
         )
-        s3.delete_object(
+        s3_client.delete_object(
             Bucket=bucket_name,
             Key="pytest_pca_ad/v0.0.1/models/npy_models/train_pca_ad_model_v0.0.1_pcaDummyDataTrain.npy",
         )
     except Exception as error_message:
         errors.append(error_message)
 
-    assert len(errors) == 0 , f"{errors}"
+    assert len(errors) == 0, f"{errors}"
