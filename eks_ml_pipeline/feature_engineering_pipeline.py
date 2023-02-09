6ccd86decac43a77be4507d8d23c6a2bb4c06592
Aakash Agrawal <aakash.agrawal@dish.com>
2023-02-09 23:16:02 +0000

    upgrade dockerfile

--- eks_ml_pipeline/feature_engineering_pipeline.py
+++ eks_ml_pipeline/feature_engineering_pipeline.py
@@ -170,7 +170,8 @@ class FeatureEngineeringPipeline:
         emr_entry_point = 's3://dish-5g.core.pd.g.dp.eks.logs.e/emr_serverless/code/emr_entry_point/emr_job.py'
         #emr_entry_point = 's3://dish-5g.core.pd.g.dp.eks.logs.e/emr_serverless/code/spark_dependency/pyspark_deps_github.tar.gz'
         # emr_entry_point = 'local:/home/hadoop/environment/bin/python3.7/site-packages/eks_ml_pipeline/emr_job.py'
-        #emr_entry_point = 'local:///site-packages/eks_ml_pipeline/emr_job.py'
+        #emr_entry_point = 'environment/lib64/python3.7/site-packages/eks_ml_pipeline/emr_job.py'
+        
         
         
         #zipped_env_path = f's3://{s3_bucket_name}/emr_serverless/code/spark_dependency/pyspark_deps_github.tar.gz'
