74833339fce3c1556ef298f6da2aaaccd5bdc653
Aakash Agrawal <aakash.agrawal@dish.com>
2023-02-10 23:00:30 +0000

    update feature engineering

--- eks_ml_pipeline/feature_engineering_pipeline.py
+++ eks_ml_pipeline/feature_engineering_pipeline.py
@@ -29,22 +29,49 @@ class FeatureEngineeringPipeline:
         self.rec_type = rec_type
         self.compute_type = compute_type
         self.aggregation_column = aggregation_column
+        
+        self.s3_utilities = None
+        self.file_name = None
+        self.create_file_path()
+        self.initialize_s3()
+        
+    def initialize_s3(self):
+        """Initialize s3 utilities class"""
+        self.s3_utilities = S3Utilities(
+            bucket_name = self.bucket, 
+            model_name = self.feature_group_name,
+            version = self.feature_version, 
+            )
 
+    def create_file_path(self):
+        if self.partition_hour == -1:
+            self.file_name = f'{self.partition_year}_{self.partition_month}_{self.partition_day}'
+        elif self.partition_day == -1:
+            self.file_name = f'{self.partition_year}_{self.partition_month}'
+        else:
+            self.file_name = f'{self.partition_year}_{self.partition_month}_{self.partition_day}_{self.partition_hour}'
+        # if rec_type == 'Node':
+        #     aggregation_column = 'InstanceId'
+        # elif rec_type == 'Container':
+        #     aggregation_column = 'container_name_pod_id'
+        # else:
+        #     aggregation_column = 'pod_id'
+    
     def run_preproceesing(self):
 
         print(self.bucket_name_raw_data)
         print(self.folder_name_raw_data)
 
         features_data, processed_data = rec_type_ad_preprocessing(rec_type=self.rec_type,
-                                                                  input_bucket_name=self.bucket_name_raw_data,
-                                                                  input_folder_name=self.folder_name_raw_data,
                                                                   input_feature_group_name=self.feature_group_name,
                                                                   input_feature_group_version=self.feature_version,
                                                                   input_year=self.partition_year,
                                                                   input_month=self.partition_month,
                                                                   input_day=self.partition_day,
                                                                   input_hour=self.partition_hour,
-                                                                  input_setup=self.spark_config_setup)
+                                                                  input_setup=self.spark_config_setup,
+                                                                  bucket_name_raw_data=self.bucket_name_raw_data,
+                                                                  folder_name_raw_data=self.folder_name_raw_data)
 
         print(f'processed data columns: {processed_data.columns}')
         print('caching')
@@ -63,25 +90,34 @@ class FeatureEngineeringPipeline:
         test_split = round(1 - train_split, 2)
         train_data, test_data = all_rectypes_train_test_split(processed_data,
                                                               [train_split, test_split])
+        print(test_data.show(1,vertical=True))
 
+       
         # initializing s3 utils
-        s3_utils = S3Utilities(self.bucket, self.feature_group_name, self.feature_version)
+        #s3_utils = S3Utilities(self.bucket, self.feature_group_name, self.feature_version)
+        
+        print(self.bucket)
+        print(self.feature_group_name)
+        print(self.feature_version)
+        print(self.file_name)
 
         # writing dfs to s3 bucket
         print('saving pre-processed to s3')
-        # processed_data.write.mode("overwrite").parquet(f's3a://{output_bucket_name}/{feature_group_name}/{feature_version}/data/spark_df/raw_data_{file_name}/')
-        s3_utils.pyspark_write_parquet(processed_data, 'data/spark_df/', 'raw_data_from_run/')
+
+        #self.s3_utilities.pyspark_write_parquet(processed_data, 'data/spark_df', f'raw_data_{self.file_name}')
+        
+        processed_data.write.mode('overwrite').parquet(f's3a://{self.bucket}/{self.feature_group_name}/{self.feature_version}/data/spark_df/raw_{self.file_name}/')
 
         print('saving features data to s3')
-        s3_utils.awswrangler_pandas_dataframe_to_s3(features_data, "data", "pandas_df",
-                                                    f'raw_features_rom_run.parquet')
+        self.s3_utilities.awswrangler_pandas_dataframe_to_s3(features_data, "data", "pandas_df",
+                                                    f'raw_features_{self.file_name}.parquet')
         print('saving raw train to s3')
-        # train_data.write.mode("overwrite").parquet(f's3a://{output_bucket_name}/{feature_group_name}/{feature_version}/data/spark_df/raw_training_data_{file_name}/')
-        s3_utils.pyspark_write_parquet(train_data, 'data/spark_df/', f'raw_training_data_from_run/')
+
+        self.s3_utilities.pyspark_write_parquet(train_data, 'data/spark_df', f'raw_training_data_{self.file_name}')
 
         print('saving raw test to s3')
-        # test_data.coalesce(10).write.mode("overwrite").parquet(f's3a://{output_bucket_name}/{feature_group_name}/{feature_version}/data/spark_df/raw_testing_data_{file_name}/')
-        s3_utils.pyspark_write_parquet(train_data, 'data/spark_df/', f'raw_testing_data_from_run/')
+
+        self.s3_utilities.pyspark_write_parquet(train_data, 'data/spark_df/', f'raw_testing_data_{self.file_name}')
 
         # un-persisting processed data
         processed_data.unpersist()
@@ -91,17 +127,17 @@ class FeatureEngineeringPipeline:
         spark = Spark_Utils().get_spark()
 
         train_data = spark.read.parquet(
-            f's3a://{self.bucket}/{self.feature_group_name}/{self.feature_version}/data/spark_df/raw_training_data_from_run/')
+            f's3a://{self.bucket}/{self.feature_group_name}/{self.feature_version}/data/spark_df/raw_training_data_{self.file_name}/')
         print(f'train data shape: {train_data.columns}')
 
         test_data = spark.read.parquet(
-            f's3a://{self.bucket}/{self.feature_group_name}/{self.feature_version}/data/spark_df/raw_testing_data_from_run/')
+            f's3a://{self.bucket}/{self.feature_group_name}/{self.feature_version}/data/spark_df/raw_testing_data_{self.file_name}/')
         print(f'train data shape: {test_data.columns}')
 
-        s3_utils = S3Utilities(self.bucket, self.feature_group_name, self.feature_version)
+        #s3_utils = S3Utilities(self.bucket, self.feature_group_name, self.feature_version)
 
         #features_data = s3_utils.read_parquet_to_pandas_df("data", "pandas_df", f'raw_features_{file_name}.parquet')
-        features_data = s3_utils.read_parquet_to_pandas_df("data", "pandas_df", 'raw_features_rom_run.parquet')
+        features_data = self.s3_utilities.read_parquet_to_pandas_df("data", "pandas_df", f'raw_features_{self.file_name}.parquet')
 
         # parsing model parameters
         scaled_features = []
@@ -151,15 +187,16 @@ class FeatureEngineeringPipeline:
 
         print('Writing train df to s3')
         training_df.coalesce(20).write.mode("overwrite").parquet(
-            's3://emr-serverless-output-pd/emr_serverless_demo/container_autoencoder_ad/v0'
-            '.0.1/data/spark_df/training_data/')
+            f's3://{self.bucket}/{self.feature_group_name}/{self.feature_version}/data/spark_df/training_data_{self.file_name}/')
         print('Un-persisting')
         train_data.unpersist()
         print("training and testing data_builder completed successfully")
 
 
     def run_in_sagemaker(self):
+        print('running data pre-processing step')
         self.run_preproceesing()
+        print('running data engineering step')
         self.run_feature_engineering()
         
     def run_in_emr(self):
@@ -171,8 +208,6 @@ class FeatureEngineeringPipeline:
         #emr_entry_point = 's3://dish-5g.core.pd.g.dp.eks.logs.e/emr_serverless/code/spark_dependency/pyspark_deps_github.tar.gz'
         # emr_entry_point = 'local:/home/hadoop/environment/bin/python3.7/site-packages/eks_ml_pipeline/emr_job.py'
         #emr_entry_point = 'environment/lib64/python3.7/site-packages/eks_ml_pipeline/emr_job.py'
-        
-        
         
         #zipped_env_path = f's3://{s3_bucket_name}/emr_serverless/code/spark_dependency/pyspark_deps_github.tar.gz'
         
