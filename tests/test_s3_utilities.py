bcbb70935a7d03570f17db38494febadc414dc36
David Cherney <david.cherney@dish.com>
2023-02-10 19:11:53 +0000

    linted

--- tests/test_s3_utilities.py
+++ tests/test_s3_utilities.py
@@ -1,11 +1,10 @@
 """Test the methods of the class S3Utilities in utilities/s3_utilities.py."""
 import os
-from eks_ml_pipeline import S3Utilities
-import numpy as np
-import pandas as pd
-import boto3
 import tempfile
 import shutil
+import numpy as np
+import pandas as pd
+from eks_ml_pipeline import S3Utilities
 
 def test_upload_file(
     ae_train_input, # for instantiating the S3Utilities class
@@ -40,7 +39,7 @@ def test_upload_file(
         Key = "pytest_s3_utilities/" + filename
         )
     # HTTP status code 200 indicates request succeeded
-    assert head['ResponseMetadata']['HTTPStatusCode'] == 200 
+    assert head['ResponseMetadata']['HTTPStatusCode'] == 200
 
     # Cleanup by deleting file from s3.
     s3_util.client.delete_object(
@@ -48,10 +47,7 @@ def test_upload_file(
         Key = "pytest_s3_utilities/" + filename
         )
 
-def test_download_file(
-    ae_train_input, # for instantiating the S3Utilities class
-    bucket_name
-    ):
+def test_download_file(bucket_name):
     """Download with method under test, and check for file locally."""
     local_dir = tempfile.mkdtemp()
     local_fname = 'local_test_download_file.npy'
@@ -76,10 +72,7 @@ def test_download_file(
     shutil.rmtree(local_dir) # remove tempdir
 
 
-def test_download_zip_and_unzip(
-    ae_train_input, # for instantiating the S3Utilities class
-    bucket_name
-    ):
+def test_download_zip_and_unzip(bucket_name):
     """Downlad a zip, check that it is local, unzip, and check for contents."""
 
     # Instantiate the class with fixtures from conftest.py.
@@ -93,12 +86,12 @@ def test_download_zip_and_unzip(
     tmpdir = tempfile.mkdtemp()
     local_fname = 'local_test_download_zip_file.zip' # what to locally name .zip file
     local_path = f'{tmpdir}/{local_fname}'  # local destination + name
-    s3_fname = "test_download_zip_file.zip" # test data in s3 
+    s3_fname = "test_download_zip_file.zip" # test data in s3
     s3_util.download_zip(
             local_path = local_path,
             folder = 'folder', # directory in s3 bucket
             type_ = "type", # directory in s3 bucket
-            file_name = s3_fname # test data in s3 
+            file_name = s3_fname # test data in s3
             )
     # test .download_zip method
     # i.e. that the .zip file is local
@@ -108,9 +101,9 @@ def test_download_zip_and_unzip(
     # i.e. that sfile is been extracted
     s3_util.unzip(path_to_zip = local_path)
     unziped_dir = tmpdir # directory where the files are placed
-    local_file_list = os.listdir(unziped_dir) # files in that directory 
+    local_file_list = os.listdir(unziped_dir) # files in that directory
     # Two files were generated for this test and zipped into s3_fname
-    # 'test_download_zip_file_1.txt' and 'test_download_zip_file_2.txt' 
+    # 'test_download_zip_file_1.txt' and 'test_download_zip_file_2.txt'
     test_file_names = ['test_download_zip_file_1.txt',
                        'test_download_zip_file_2.txt']
     test_bools = [file_name in local_file_list for file_name in test_file_names]
@@ -139,7 +132,7 @@ def test_zip_and_upload(bucket_name):
         local_path = tmpdir, #directory containing the files to zip
         folder = 'folder',
         type_ = "type",
-        file_name = s3_filename 
+        file_name = s3_filename
         )
 
     # check that file is in s3
@@ -148,9 +141,9 @@ def test_zip_and_upload(bucket_name):
         Key = ("pytest_s3_utilities/version/folder/type/" + s3_filename)
         )
     # HTTP status code 200 indicates request succeeded
-    assert head['ResponseMetadata']['HTTPStatusCode'] == 200 
+    assert head['ResponseMetadata']['HTTPStatusCode'] == 200
 
-    # Cleanup: 
+    # Cleanup:
     # delete uploaded file from s3
     s3_util.client.delete_object(
         Bucket=bucket_name,
@@ -159,7 +152,7 @@ def test_zip_and_upload(bucket_name):
     # delete temp folder
     shutil.rmtree(tmpdir) # remove tempdir
 
-    
+
 def test_pandas_dataframe_to_s3(bucket_name):
     """Create df, upload with method uner test, and check for file in S3."""
     # Instantiate the class with fixtures from conftest.py.
@@ -171,12 +164,12 @@ def test_pandas_dataframe_to_s3(bucket_name):
 
     # create pandas df in memory
     data = {'col1': [1, 2], 'col2': [3, 4]}
-    df = pd.DataFrame(data=data)
+    data_frame = pd.DataFrame(data=data)
 
     # upload to s3 with method under test
-    pandas_file_name = 'test_pandas_to_s3.parquet' 
+    pandas_file_name = 'test_pandas_to_s3.parquet'
     s3_util.pandas_dataframe_to_s3(
-        input_datafame = df, 
+        input_datafame = data_frame,
         folder = 'folder', # destination dir
         type_  = "type", #destination dir
         file_name = pandas_file_name
@@ -189,7 +182,7 @@ def test_pandas_dataframe_to_s3(bucket_name):
         )
 
     # HTTP status code 200 indicates request succeeded
-    assert head['ResponseMetadata']['HTTPStatusCode'] == 200 
+    assert head['ResponseMetadata']['HTTPStatusCode'] == 200
 
     # Cleanup : delete file from s3
     s3_util.client.delete_object(
@@ -212,7 +205,7 @@ def test_write_tensor(bucket_name):
     # write to s3 with method
     numpy_file_name = 'test_numpy_to_s3.npy'
     s3_util.write_tensor(
-        tensor = test_tensor, 
+        tensor = test_tensor,
         folder = 'folder', # destination dir
         type_  = "type", # destination dir
         file_name = numpy_file_name
@@ -224,7 +217,7 @@ def test_write_tensor(bucket_name):
         Key = "pytest_s3_utilities/version/folder/type/" + numpy_file_name
         )
     # HTTP status code 200 indicates request succeeded
-    assert head['ResponseMetadata']['HTTPStatusCode'] == 200 
+    assert head['ResponseMetadata']['HTTPStatusCode'] == 200
 
     # delete file from s3
     s3_util.client.delete_object(
@@ -243,12 +236,12 @@ def test_awswrangler_pandas_dataframe_to_s3(bucket_name):
 
     # generate pandas df in memory
     data = {'col1': [1, 2], 'col2': [3, 4]}
-    df = pd.DataFrame(data=data)
+    data_frame = pd.DataFrame(data=data)
 
     # upload to s3 with the method under test
     pandas_file_name = 'test_wrangler_pandas_to_s3.parquet'
     s3_util.awswrangler_pandas_dataframe_to_s3(
-        input_datafame = df, 
+        input_datafame = data_frame,
         folder = 'folder', # destination dir
         type_  = "type", # destination dir
         file_name = pandas_file_name
@@ -259,14 +252,14 @@ def test_awswrangler_pandas_dataframe_to_s3(bucket_name):
         Key = "pytest_s3_utilities/version/folder/type/" + pandas_file_name
         )
     # HTTP status code 200 indicates request succeeded
-    assert head['ResponseMetadata']['HTTPStatusCode'] == 200 
+    assert head['ResponseMetadata']['HTTPStatusCode'] == 200
 
     # Clean up:  delete file from s3
     s3_util.client.delete_object(
         Bucket = bucket_name,
         Key = "pytest_s3_utilities/version/folder/type/" + pandas_file_name
         )
-    
+
 def test_read_tensor(bucket_name):
     """Download tensor from S3 with method under test, check tensor is in memory."""
     # Instantiate the class with fixtures from conftest.py.
@@ -287,8 +280,8 @@ def test_read_tensor(bucket_name):
     assert test_tensor.shape == (2,3,4,5)
 
 def test_upload_directory(bucket_name):
-    """Create a directory, place two files in that directory, 
-    upload all files in that directory with method under test, 
+    """Create a directory, place two files in that directory,
+    upload all files in that directory with method under test,
     and check that both files have been uploaded.
     """
     # Instantiate the class with fixtures from conftest.py.
@@ -307,9 +300,9 @@ def test_upload_directory(bucket_name):
 
     # use method under test to upload dir with contents
     s3_util.upload_directory(
-        local_path = test_directory, 
-        folder = 'folder', 
-        type_  = "type", 
+        local_path = test_directory,
+        folder = 'folder',
+        type_  = "type",
         )
 
 
@@ -324,7 +317,7 @@ def test_upload_directory(bucket_name):
 
     assert all(test_bools)
 
-    # Clean up: 
+    # Clean up:
     # delete files from s3
     for fname in fnames:
         s3_util.client.delete_object(
@@ -332,15 +325,15 @@ def test_upload_directory(bucket_name):
             Key = ("pytest_s3_utilities/version/folder/type/" + fname)
             )
     # remove tempdir
-    shutil.rmtree(test_directory) 
+    shutil.rmtree(test_directory)
 
-#### NOTE ####      
-# test_pyspark_write_parquet() need not exist 
-# because it would test only a pyspark funcion.  
+#### NOTE ####
+# test_pyspark_write_parquet() need not exist
+# because it would test only a pyspark funcion.
 
-    
+
 def test_read_parquet_to_pandas_df(bucket_name):
-    """Use the method under test to put dataframe into memory, 
+    """Use the method under test to put dataframe into memory,
     and check that dataframe is in memory.
     """
     s3_util = S3Utilities(
@@ -352,8 +345,8 @@ def test_read_parquet_to_pandas_df(bucket_name):
     # read test parquet with method under test
     parquet_file_name = 'test_read_parquet_to_pandas_df.parquet'
     test_df = s3_util.read_parquet_to_pandas_df(
-        folder = 'folder', 
-        type_  = "type", 
+        folder = 'folder',
+        type_  = "type",
         file_name = parquet_file_name)
 
     # check that tensor is in memory
