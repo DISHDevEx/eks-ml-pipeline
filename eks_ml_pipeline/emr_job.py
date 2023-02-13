0613b070156a5debaba8c436ee6fb69750c2e776
Aakash Agrawal <aakash.agrawal@dish.com>
2023-02-13 16:15:49 -0700

    workflow updates

--- eks_ml_pipeline/emr_job.py
+++ eks_ml_pipeline/emr_job.py
@@ -6,13 +6,13 @@ from eks_ml_pipeline import node_autoencoder_fe_input
 
 
 if __name__ == "__main__":
-    # load_dotenv("/run/secrets/")
+    load_dotenv("")
 
     rec_type = 'Node'
     compute_type = 'sagemaker'
     aggregation_column = 'InstanceId'
     input_data_type = 'test'
 
-    dpfep = FeatureEngineeringPipeline(node_autoencoder_fe_input(), aggregation_column, rec_type, compute_type, input_data_type)
+    fep = FeatureEngineeringPipeline(node_autoencoder_fe_input(), rec_type, compute_type, input_data_type)
 
-    dpfep.run_preproceesing()
+    fep.run_preproceesing()
