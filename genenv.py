b59b8f05361387dedbf0e19d2acea1b55d0c33c5
Aakash Agrawal <aakash.agrawal@dish.com>
2023-02-13 13:59:40 -0700

    update workflow for emr

---
+++ genenv.py
@@ -0,0 +1,16 @@
+import os
+def get_config_from_environment(env_content):
+    env_content += "MYSQL_HOST={}\n".format(os.environ.get("BUCKET_NAME_RAW_DATA", ""))
+    env_content += "MYSQL_USER={}\n".format(os.environ.get("FOLDER_NAME_RAW_DATA", ""))
+    name1 = os.environ.get("BUCKET_NAME_RAW_DATA", "")
+    name2 = os.environ.get("FOLDER_NAME_RAW_DATA", "")
+    print(name1)
+    print(name2)
+    return env_content
+
+
+
+env_content = ""
+env_content = get_config_from_environment(env_content)
+with open(".env", "w", encoding="utf8") as env:
+    env.write(env_content)
