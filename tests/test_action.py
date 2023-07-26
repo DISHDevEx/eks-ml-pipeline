011e905bd03b3b4343bc946a46bcfe0ef199ef7a
Aakash Agrawal <aakash.agrawal@dish.com>
2023-07-26 23:12:47 +0000

    trigger 2

--- tests/test_action.py
+++ tests/test_action.py
@@ -8,26 +8,39 @@ import numpy as np
 import pandas as pd
 from eks_ml_pipeline import S3Utilities
 
-def test_download_file(bucket_name):
-    """Download with method under test, and check for file locally."""
-    local_dir = tempfile.mkdtemp()
-    local_fname = 'local_test_download_file.npy'
-    local_path = f'{local_dir}/{local_fname}'
 
+    
+def test_write_tensor(bucket_name):
+    """Create tensor, upload with method under test, and check for file in S3"""
     # Instantiate the class with fixtures from conftest.py.
     s3_util = S3Utilities(
         bucket_name = bucket_name,
-        model_name = '', # irrelevant for this test
-        version = '' # irrelevant for this test
+        model_name = 'pytest_s3_utilities', # destination dir
+        version = 'version', # destination dir
         )
-    # download file
-    s3_util.download_file(
-        local_path = local_path,
-        bucket_name = bucket_name,
-        key = "pytest_s3_utilities/test_download_file.npy",
-        )
-    # check that file is local
-    assert local_fname in os.listdir(local_dir)
 
-    # Cleanup
-    shutil.rmtree(local_dir) # remove tempdir
+    # generate tensor
+    test_tensor = np.zeros((2,3,4,5))
+
+    # write to s3 with method
+    numpy_file_name = 'test_numpy_to_s3.npy'
+    s3_util.write_tensor(
+        tensor = test_tensor,
+        folder = 'folder', # destination dir
+        type_  = "type", # destination dir
+        file_name = numpy_file_name
+        )
+
+    # check that file is in s3
+    head = s3_util.client.head_object(
+        Bucket=bucket_name,
+        Key = "pytest_s3_utilities/version/folder/type/" + numpy_file_name
+        )
+    # HTTP status code 200 indicates request succeeded
+    assert head['ResponseMetadata']['HTTPStatusCode'] == 200
+
+    # delete file from s3
+    s3_util.client.delete_object(
+        Bucket=bucket_name,
+        Key = "pytest_s3_utilities/version/folder/type/" + numpy_file_name
+        )
