a85d4f2b5f7068a96511b76edce1e5ab2649cd33
Aakash Agrawal <aakash.agrawal@dish.com>
2023-07-30 18:22:08 +0000

    test upload file function

--- tests/test_action.py
+++ tests/test_action.py
@@ -9,38 +9,43 @@ import pandas as pd
 from eks_ml_pipeline import S3Utilities
 
 
-    
-def test_write_tensor(bucket_name):
-    """Create tensor, upload with method under test, and check for file in S3"""
+def test_upload_file(
+    ae_train_input, # for instantiating the S3Utilities class
+    bucket_name
+    ):
+    """Upload file wtih method under test, and check for uploaded file."""
+
+    # generate a file to be uploaded and save it in a temp path
+    filename = 'test_upload_file.npy'
+    tmpdir = tempfile.mkdtemp()
+    file_path = f'{tmpdir}/{filename}'
+
+    np.save(file_path, np.array([1,2,3]))
+
     # Instantiate the class with fixtures from conftest.py.
     s3_util = S3Utilities(
         bucket_name = bucket_name,
-        model_name = 'pytest_s3_utilities', # destination dir
-        version = 'version', # destination dir
+        model_name = ae_train_input[1][0], #feature_selection[0] = feature_group_name
+        version = ae_train_input[1][1], #feature_selection[1] =  feature_input_version
         )
 
-    # generate tensor
-    test_tensor = np.zeros((2,3,4,5))
-
-    # write to s3 with method
-    numpy_file_name = 'test_numpy_to_s3.npy'
-    s3_util.write_tensor(
-        tensor = test_tensor,
-        folder = 'folder', # destination dir
-        type_  = "type", # destination dir
-        file_name = numpy_file_name
+    # use the method under test to upload that file to s3
+    s3_util.upload_file(
+        local_path = file_path,
+        bucket_name = bucket_name,
+        key = "pytest_s3_utilities/" + filename
         )
 
-    # check that file is in s3
+    # test that the file is in s3
     head = s3_util.client.head_object(
-        Bucket=bucket_name,
-        Key = "pytest_s3_utilities/version/folder/type/" + numpy_file_name
+        Bucket = bucket_name,
+        Key = "pytest_s3_utilities/" + filename
         )
     # HTTP status code 200 indicates request succeeded
     assert head['ResponseMetadata']['HTTPStatusCode'] == 200
 
-    # delete file from s3
+    # Cleanup by deleting file from s3.
     s3_util.client.delete_object(
         Bucket=bucket_name,
-        Key = "pytest_s3_utilities/version/folder/type/" + numpy_file_name
+        Key = "pytest_s3_utilities/" + filename
         )
